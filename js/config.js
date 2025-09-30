0db65b571c4ccaec7fb26e6f550ce60de3f554cf
Alexander O Ilevbare <153298674+Omoikhoa@users.noreply.github.com>
2025-09-30 08:01:04 -0400

    Change userPoolClientId in config.js
    Updated userPoolClientId in the config.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_bTwiBH9j2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7vv1mrvmpmvgur8ilsmm53pdo6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '73pagmos02fcguv281iuqfl6kf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
